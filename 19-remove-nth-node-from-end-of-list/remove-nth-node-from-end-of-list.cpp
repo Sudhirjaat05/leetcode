cdc8031461dcc6a568c548f32f891df9bb6173fd
Sudhirjaat05 <2025ucp1780@mnit.ac.in>
2026-08-09 19:28:05 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (93.96%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,24 +11,38 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head->next==NULL){
-            head=head->next;
-            return head;
+        // if(head->next==NULL){
+        //     head=head->next;
+        //     return head;
+        // }
+        // int size=1;
+        // ListNode* temp=head;
+        // ListNode* tail=head;
+        // while(tail->next!=NULL){
+        //     tail=tail->next;
+        //     size++;
+        // }
+        // if(size-n==0) head=head->next;
+        // for(int i=1;i<(size-n);i++){
+        //     temp=temp->next;
+        // }
+        // temp->next=temp->next->next;
+        // return head;
+ 
+    // Using slow and faster technique
+        // traverse fast for n-+1 times
+        ListNode* slow=head;
+        ListNode* fast=head;
+        for(int i=1;i<=n+1;i++){
+            if(fast==NULL) return head->next;
+            fast=fast->next;
         }
-        int size=1;
-        ListNode* temp=head;
-        ListNode* tail=head;
-        while(tail->next!=NULL){
-            tail=tail->next;
-            size++;
+        // move slow and fast 1 steps together
+        while(fast!=NULL){
+            slow=slow->next;
+            fast=fast->next;
         }
-        if(size-n==0) head=head->next;
-        for(int i=1;i<(size-n);i++){
-            temp=temp->next;
-        }
-        temp->next=temp->next->next;
+        slow->next=slow->next->next;
         return head;
-
-
     }
 };

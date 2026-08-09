b2a044ca029b04cd4aca8bc93dd9731685e8cc4c
Sudhirjaat05 <2025ucp1780@mnit.ac.in>
2026-08-09 18:57:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (65.04%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -11,17 +11,24 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* temp=head;
-        int size=1;
-        while(temp->next!=NULL){
-            temp=temp->next;
-            size++;
+        // ListNode* temp=head;
+        // int size=1;
+        // while(temp->next!=NULL){
+        //     temp=temp->next;
+        //     size++;
+        // }
+        // int idx=(size/2)+1;
+        // for(int i=1;i<idx;i++){
+        //     head=head->next;
+        // }
+        // return head;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){ // condition should be linewise, cz we cant check fast->next before fast bcz if fast is null then its next not contain null , so we do check linewise condn
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        int idx=(size/2)+1;
-        for(int i=1;i<idx;i++){
-            head=head->next;
-        }
-        return head;
+        return slow;
 
     }
 };

32e112e2f685e59d610dbf9e4f55033004ad25c2
Sudhirjaat05 <2025ucp1780@mnit.ac.in>
2026-08-11 18:47:57 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (99.77%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -12,18 +12,32 @@ class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if(head==NULL || head->next==NULL) return head;
-        ListNode* temp=head;
-        ListNode* curr=head->next;
-        while(curr!=NULL){
-            if(curr->val==temp->val) curr=curr->next;
-            else {
-                temp=temp->next;
-                swap(temp->val,curr->val);
-                curr=curr->next;
+        // ListNode* temp=head;
+        // ListNode* curr=head->next;
+        // while(curr!=NULL){
+        //     if(curr->val==temp->val) curr=curr->next;
+        //     else {
+        //         temp=temp->next;
+        //         swap(temp->val,curr->val);
+        //         curr=curr->next;
+        //     }
+        // }
+        // temp->next=NULL;
+        // temp=head;
+        // return temp;
+        
+        // if u dont want to use swap then use this
+
+        ListNode* a=head;
+        ListNode* b=head->next;
+        while(b!=NULL){
+            while(b!=NULL && b->val==a->val){
+                b=b->next;
             }
+            a->next=b;
+            a=b;
+            if(b!=NULL) b=b->next;
         }
-        temp->next=NULL;
-        temp=head;
-        return temp;
+        return head;
     }
 };
